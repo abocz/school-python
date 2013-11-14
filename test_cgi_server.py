d5ac1711715f533b66ea4925c9169dd0078fe726
Andrew <aboczjr@gmail.com>
2013-11-13 20:37:06 -0500

    added server for testing cgi scripts

---
+++ test_cgi_server.py
@@ -0,0 +1,10 @@
+#test server,  python3.3
+from http.server import HTTPServer, CGIHTTPRequestHandler
+
+
+class Handler(CGIHTTPRequestHandler):
+    cgi_directories = ["/cgi-bin"]
+PORT = 8080
+httpd = HTTPServer(("", PORT), Handler)
+print("serving at port", PORT)
+httpd.serve_forever()
